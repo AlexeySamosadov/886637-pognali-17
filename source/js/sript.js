11e438049f490a966d3d0fac920cf8ec7c403520
AlexeySamosadov <asamosadov@gmail.com>
2019-08-13 00:23:02 +0300

    Сразложил на сетке пару секций

--- source/js/sript.js
+++ source/js/sript.js
@@ -1,17 +1,20 @@
-var menuButton = document.querySelector(".main-nav__toggle--open")
-var menuButtonClose = document.querySelector(".main-nav__toggle--close");
+var menuButton = document.querySelector(".main-nav__toggle")
 var menuWrap = document.querySelector(".main-nav__wrapper");
+var svgOpen = document.querySelector(".main-nav__toggle-svg--open");
+var svgClose = document.querySelector(".main-nav__toggle-svg--close");
 
 menuButton.addEventListener("click", function() {
-  menuWrap.classList.add("display-flex");
+  menuWrap.classList.toggle("main-nav__wrapper--opened");
+  menuButton.classList.toggle("main-nav__toggle--open");
+  svgOpen.classList.toggle("main-nav__toggle-svg--open-modal");
+  svgClose.classList.toggle("main-nav__toggle-svg--close-modal");
 });
-
-menuButtonClose.addEventListener("click", function () {
-  menuWrap.classList.remove("display-flex");
-})
 
 document.addEventListener("keydown", function(event){
   if(event.keyCode == 27) {
-    menuWrap.classList.remove("display-flex");
+    menuWrap.classList.remove("main-nav__wrapper--opened");
+    menuButton.classList.remove("main-nav__toggle--open");
+    svgOpen.classList.remove("main-nav__toggle-svg--open-modal");
+    svgClose.classList.remove("main-nav__toggle-svg--close-modal");
   }
 });
