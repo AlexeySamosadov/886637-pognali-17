4e53136748649aaa7346c90816b2e8ea7da18479
AlexeySamosadov <asamosadov@gmail.com>
2019-08-29 23:44:41 +0300

    Подключил к js всплывающий фильтр

--- source/js/sript.js
+++ source/js/sript.js
@@ -2,6 +2,9 @@ var menuButton = document.querySelector(".main-nav__toggle")
 var menuWrap = document.querySelector(".main-nav__wrapper");
 var svgOpen = document.querySelector(".main-nav__toggle-svg--open");
 var svgClose = document.querySelector(".main-nav__toggle-svg--close");
+var filter = document.querySelector(".filter-by-country__filter-title");
+var filterBlock = document.querySelector(".filter-by-country__block");
+
 
 menuButton.addEventListener("click", function() {
   menuWrap.classList.toggle("main-nav__wrapper--opened");
@@ -16,5 +19,10 @@ document.addEventListener("keydown", function(event){
     menuButton.classList.remove("main-nav__toggle--open");
     svgOpen.classList.remove("main-nav__toggle-svg--open-modal");
     svgClose.classList.remove("main-nav__toggle-svg--close-modal");
+    filterBlock.classList.remove("filter-by-country__block--active")
   }
+});
+
+filter.addEventListener("click", function(event) {
+  filterBlock.classList.toggle("filter-by-country__block--active")
 });
