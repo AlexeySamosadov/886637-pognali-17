e60ad0729cc4a7cbc589b1c32682c14e8872388b
AlexeySamosadov <asamosadov@gmail.com>
2019-08-30 15:18:13 +0300

    Сделал всплывающее окно каталога в планшетной версии

--- source/js/sript.js
+++ source/js/sript.js
@@ -2,10 +2,11 @@ var menuButton = document.querySelector(".main-nav__toggle")
 var menuWrap = document.querySelector(".main-nav__wrapper");
 var svgOpen = document.querySelector(".main-nav__toggle-svg--open");
 var svgClose = document.querySelector(".main-nav__toggle-svg--close");
-var filter = document.querySelector(".filter-by-country__filter");
+var filter = document.querySelector(".filter-by-country__filter-title");
+var filterShowAll = document.querySelector (".filter-by-country__filter-title--show-all");
 var filterBlock = document.querySelector(".filter-by-country__block");
 var filterButtonClose = filterBlock.querySelector(".filter-by-country__button-close");
-
+var filterRegionList = document.querySelector(".filter-by-country__region-list");
 
 menuButton.addEventListener("click", function() {
   menuWrap.classList.toggle("main-nav__wrapper--opened");
@@ -20,12 +21,18 @@ document.addEventListener("keydown", function(event){
     menuButton.classList.remove("main-nav__toggle--open");
     svgOpen.classList.remove("main-nav__toggle-svg--open-modal");
     svgClose.classList.remove("main-nav__toggle-svg--close-modal");
-    filterBlock.classList.remove("filter-by-country__block--active")
+    filterBlock.classList.remove("filter-by-country__block--active");
   }
 });
 
 filter.addEventListener("click", function(event) {
-  filterBlock.classList.toggle("filter-by-country__block--active")
+  filterBlock.classList.toggle("filter-by-country__block--active");
+  filterRegionList.classList.toggle("filter-by-country__region-list--active");
+});
+
+filterShowAll.addEventListener("click", function () {
+  filterBlock.classList.toggle("filter-by-country__block--active");
+  filterRegionList.classList.toggle("filter-by-country__region-list--active");
 });
 
 filterButtonClose.addEventListener("click", function(){
