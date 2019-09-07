1aea71eb88c8a66994d962081fce0226d1cd6546
AlexeySamosadov <asamosadov@gmail.com>
2019-09-07 03:50:37 +0300

    Добавил попап и js к нему также исправил ошибки js

--- source/js/sript.js
+++ source/js/sript.js
@@ -11,6 +11,11 @@ var showAllSpan = document.querySelector (".filter-by-country__show-all");
 var showAllIcon = document.querySelector (".filter-by-country__filter-icon3");
 var showAllClose = document.querySelector (".filter-by-country__close");
 var showAllIconClose = document.querySelector (".filter-by-country__svg-close");
+var form = document.querySelector(".add-plan__form");
+var tariffButton = document.querySelector(".add-profile__show-all-tariff");
+var tariffPopup = document.querySelector(".add-profile-popup");
+var tariffButtonClose = document.querySelector(".add-profile-popup__button-close");
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -33,28 +38,61 @@ document.addEventListener("keydown", function(event){
     menuButton.classList.remove("main-nav__toggle--open");
     svgOpen.classList.remove("main-nav__toggle-svg--open-modal");
     svgClose.classList.remove("main-nav__toggle-svg--close-modal");
-    filterBlock.classList.remove("filter-by-country__block--active");
+    tariffPopup.classList.remove("add-profile-popup--active");
   }
 });
 
-filter.addEventListener("click", function(event) {
-  filterBlock.classList.toggle("filter-by-country__block--active");
-  filterRegionList.classList.toggle("filter-by-country__region-list--active");
-});
+if(filter) {
+  filter.addEventListener("click", function(event) {
+    filterBlock.classList.toggle("filter-by-country__block--active");
+    filterRegionList.classList.toggle("filter-by-country__region-list--active");
+  });
 
-filterShowAll.addEventListener("click", function () {
-  filterBlock.classList.toggle("filter-by-country__block--active");
-  filterRegionList.classList.toggle("filter-by-country__region-list--active");
-  showAllSpan.classList.toggle("filter-by-country__show-all--active");
-  showAllIcon.classList.toggle("filter-by-country__filter-icon3--active");
-  showAllClose.classList.toggle("filter-by-country__close--active");
-  showAllIconClose.classList.toggle("filter-by-country__svg-close--active");
-});
+  filterShowAll.addEventListener("click", function () {
+    filterBlock.classList.toggle("filter-by-country__block--active");
+    filterRegionList.classList.toggle("filter-by-country__region-list--active");
+    showAllSpan.classList.toggle("filter-by-country__show-all--active");
+    showAllIcon.classList.toggle("filter-by-country__filter-icon3--active");
+    showAllClose.classList.toggle("filter-by-country__close--active");
+    showAllIconClose.classList.toggle("filter-by-country__svg-close--active");
+  });
 
-filterButtonClose.addEventListener("click", function() {
-  filterBlock.classList.remove("filter-by-country__block--active")
-});
+  filterButtonClose.addEventListener("click", function() {
+    filterBlock.classList.remove("filter-by-country__block--active")
+  });
 
-showAllIcon.addEventListener("clik", function () {
-  console.log("клик работает!")
-});
+  showAllIcon.addEventListener("clik", function () {
+    console.log("клик работает!")
+  });
+
+  document.addEventListener("keydown", function(event){
+    if(event.keyCode == 27) {
+      filterBlock.classList.remove("filter-by-country__block--active");
+    }
+  });
+
+}
+
+if (form) {
+  form.addEventListener("submit", function (evt) {
+    if (!textarea.value) {
+      evt.preventDefault();
+      console.log("Заполните данный формы, пожалуйста!");
+      console.log(login.value);
+      console.log(email.value);
+      console.log(textarea.value);
+    }
+  });
+}
+
+if (tariffButton) {
+  tariffButton.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    tariffPopup.classList.add("add-profile-popup--active")
+  });
+
+  tariffButtonClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    tariffPopup.classList.remove("add-profile-popup--active")
+  })
+}
